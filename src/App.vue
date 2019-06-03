76b1c185fef190cc81bb95760a11de48480b0f86
Jason <jasoncruises@gmail.com>
2019-06-04 00:27:18 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="container" id="app">
+    <img class="logo" src="./assets/logo.png" width="100">
+    <todo-list></todo-list>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+import TodoList from './components/TodoList'
+
+import Vue from 'vue'
+import BootstrapVue from 'bootstrap-vue'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(BootstrapVue)
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    TodoList
+  }
+}
+</script>
+
+<style>
+#app {
+  /* font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px; */
+}
+.logo {
+  display: block;
+  margin: 3rem auto;
+}
+</style>
